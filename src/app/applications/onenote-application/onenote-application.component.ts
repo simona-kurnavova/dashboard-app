37aeae122b5cbc00f57de36d4f3011e7367e2ace
Simona Kurňavová <kurnavova.simona@gmail.com>
2018-04-28 00:13:58 +0200

    Loading resources and UI of onenote app

--- src/app/applications/onenote-application/onenote-application.component.ts
+++ src/app/applications/onenote-application/onenote-application.component.ts
@@ -3,9 +3,22 @@ import {MAPPINGS} from '../../components/main.components/application.component';
 import {ApplicationBaseComponent} from '../application-base.component';
 import {OneNoteApplicationService} from './onenote-application.service';
 
+export interface Page {
+  id;
+  title;
+  contentUrl;
+  content?: any;
+  parentSection: {id;};
+}
+export interface Section {
+  id;
+  displayName;
+}
+
 export interface Notebook {
-  name: String;
-  sections: any[];
+  id;
+  displayName;
+  sections?: Section[];
 }
 
 @Component({
@@ -16,12 +29,27 @@ export interface Notebook {
 
 export class OneNoteApplicationComponent extends ApplicationBaseComponent implements OnInit {
   public notebookList: Notebook[] = [];
+  public activeNotebook: Notebook;
+  public activeSection: Section;
+  public activePage: Page;
+  public pageList: Page[] = [];
+  public view;
 
   constructor(private appService: OneNoteApplicationService) {
     super();
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.activeNotebook = null;
+    this.activeSection = null;
+    this.activePage = null;
+    if (this.appService.isLoggedIn()) {
+      this.setView('notebooks');
+      this.getResources();
+    } else {
+      this.setView('no account');
+    }
+  }
 
   getCode() {
     this.appService.getCode();
@@ -33,16 +61,78 @@ export class OneNoteApplicationComponent extends ApplicationBaseComponent implem
       data => {
         console.log(data);
         this.appService.saveToken(data);
+        this.getResources();
       },
       err => console.log(err)
     );
   }
 
+  getResources() {
+    this.getNotebooks();
+    this.getPages();
+  }
+
   getNotebooks() {
+    // TODO: alerts
     this.appService.getNotebooks().subscribe(
-      data => console.log(data),
+      data => {
+        this.notebookList = <Notebook[]>data['value'];
+        for (let i = 0; i < this.notebookList.length; i++) {
+          this.appService.getSections(this.notebookList[i].id).subscribe(
+            data => {
+                this.notebookList[i].sections = <Section[]>data['value'];
+            },
+            err => console.log(err)
+          );
+        }
+        console.log(this.notebookList.length);
+      },
       err => console.log(err)
     );
+  }
+
+  getPages() {
+    // TODO: alerts
+    this.appService.getAllPages().subscribe(
+      data => {
+        console.log(data);
+        this.pageList = <Page[]>data['value'];
+        for (let i = 0; i < this.pageList.length; i++) {
+          this.appService.getPage(this.pageList[i].contentUrl).subscribe(
+            data => {},
+            err => {
+              if (err.status === 200) {
+                this.pageList[i].content = err.error.text;
+              }
+            }
+          );
+        }
+      },
+      err => console.log(err)
+    );
+  }
+  openNotebook(notebook: Notebook) {
+    this.view = 'sections';
+    this.activeSection = null;
+    this.activeNotebook = notebook;
+  }
+
+  openSection(section: Section) {
+    this.view = 'pages';
+    this.activeSection = section;
+  }
+
+  openPage(page: Page) {
+    this.setView('page');
+    this.activePage = page;
+  }
+
+  setView(view: String) {
+    if (view === 'all pages' || view === 'editor' || view === 'notebooks') {
+      this.activeNotebook = null;
+      this.activeSection = null;
+    }
+    this.view = view;
   }
 }
 
--- src/app/applications/onenote-application/onenote-application.service.ts
+++ src/app/applications/onenote-application/onenote-application.service.ts
@@ -2,6 +2,7 @@ import {Injectable, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {BACKEND} from '../../settings';
 import {Observable} from 'rxjs/Observable';
+import {Notebook} from './onenote-application.component';
 
 @Injectable()
 export class OneNoteApplicationService implements OnInit {
@@ -9,7 +10,7 @@ export class OneNoteApplicationService implements OnInit {
   static URL_RESOURCES = 'https://graph.microsoft.com/v1.0/me/onenote/';
   static CLIENT_ID = '78b9d80b-aab3-4615-8a23-5864573f967a';
   static REDIRECT_URI = 'http://localhost:4200/home/';
-  static SCOPE = [ 'https://graph.microsoft.com/Notes.ReadWrite.All' ];
+  static SCOPE = [ 'https://graph.microsoft.com/Notes.ReadWrite.All'];
 
   private code: String;
 
@@ -28,14 +29,13 @@ export class OneNoteApplicationService implements OnInit {
     const cutUrl = (location.href.split('#'))[1];
     const code_string = (cutUrl.split('&'))[0];
     this.code = (code_string.split('='))[1];
-    if (this.code) {
-      console.log(this.code);
-    }
   }
 
   saveToken(data) {
+    // TODO: save token to DB and pull from it
     const expiresAt =  new Date().getTime() + (1000 * data['expires_in']);
     localStorage.setItem('onenote_access_token', data['access_token']);
+    localStorage.setItem('onenote_refresh_token', data['refresh_token']);
     localStorage.setItem('onenote_expires_at', expiresAt.toString());
   }
 
@@ -44,7 +44,7 @@ export class OneNoteApplicationService implements OnInit {
       + 'client_id=' + OneNoteApplicationService.CLIENT_ID
       + '&response_type=code'
       + '&redirect_uri=' + OneNoteApplicationService.REDIRECT_URI
-      + '&scope=' + OneNoteApplicationService.SCOPE
+      + '&scope=' + 'openid%20offline_access%20https%3A%2F%2Fgraph.microsoft.com%2FNotes.ReadWrite.All'
       + '&response_mode=fragment';
   }
 
@@ -54,15 +54,62 @@ export class OneNoteApplicationService implements OnInit {
     });
   }
 
+  refreshToken(): Observable<any> {
+    return this.http.post(BACKEND + 'onenote/refresh_token', {
+      'refresh_token': localStorage.getItem('onenote_refresh_token'),
+    });
+  }
+
   getNotebooks(): Observable<any> {
     return this.http.get(OneNoteApplicationService.URL_RESOURCES + 'notebooks',
+      {headers: this.getHeaders() });
+  }
+
+  getSections(id: number): Observable<any> {
+    // notebooks/{id}/sections
+    return this.http.get(OneNoteApplicationService.URL_RESOURCES + 'notebooks/' + id.toString() + '/sections',
+      {headers: this.getHeaders()});
+  }
+
+  getAllPages(): Observable<any> {
+    return this.http.get(OneNoteApplicationService.URL_RESOURCES + 'pages',
+      {headers: this.getHeaders()});
+  }
+
+  getPage(url: string): Observable<any> {
+    return this.http.get(url, {headers: this.getHeaders()});
+  }
+
+  createNotebook(notebook: Notebook): Observable<any> {
+    return this.http.post(OneNoteApplicationService.URL_RESOURCES + 'notebooks', {
+        'displayName': notebook.displayName
+      }
+    );
+  }
+
+  createSection(id: number, section): Observable<any> {
+    return this.http.post(OneNoteApplicationService.URL_RESOURCES + 'notebooks/' + id.toString() + '/sections',
       {headers: this.getHeaders()});
   }
 
   getHeaders(): HttpHeaders {
+    this.isLoggedIn();
     return new HttpHeaders({
       'Content-type': 'application/json; ' + 'charset=utf-8',
       'Authorization': 'Bearer ' + localStorage.getItem('onenote_access_token'),
     });
+  }
+
+  isLoggedIn(): Boolean {
+    if (!localStorage.getItem('onenote_access_token')) {
+      return false;
+    }
+    if (+localStorage.getItem('onenote_expires_at') > new Date().getTime()) {
+      return true;
+    } else {
+      this.refreshToken().subscribe(
+        data => this.saveToken(data),
+      );
+    }
   }
 }
