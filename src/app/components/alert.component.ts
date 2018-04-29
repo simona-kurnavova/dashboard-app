c0ba74ab8c5928ccbc0d36cfb4f52bd6e301dc7f
Simona Kurňavová <kurnavova.simona@gmail.com>
2018-04-29 17:57:53 +0200

    Comments to AlertComponent

--- src/app/components/alert.component.ts
+++ src/app/components/alert.component.ts
@@ -1,15 +1,24 @@
 import {Component, Input} from '@angular/core';
 import {AlertInterface} from '../authentication-alerts';
 
+/**
+ * Component using Bootstrap Alerts
+ */
+
 @Component({
   selector: 'alert',
   templateUrl: './alert.component.html',
-  providers: [],
 })
 
 export class AlertComponent {
+  /**
+   * Array of alerts
+   */
   @Input() alerts: Array<AlertInterface>;
 
+  /**
+   * Required method by Bootstrap for closing alerts
+   */
   public closeAlert(alert: AlertInterface) {
     const index: number = this.alerts.indexOf(alert);
     this.alerts.splice(index, 1);
