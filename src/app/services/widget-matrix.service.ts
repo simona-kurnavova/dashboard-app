050cf2487e79b860d78e500af3cea0e6d388990f
Simona Kurňavová <kurnavova.simona@gmail.com>
2018-04-29 18:26:17 +0200

    Widgets matrix service comments

--- src/app/services/widget-matrix.service.ts
+++ src/app/services/widget-matrix.service.ts
@@ -2,9 +2,15 @@ import {Injectable} from '@angular/core';
 import {WidgetInterface} from './widget.service';
 import {DEFAULT_WIDGET_HEIGHT, DEFAULT_WIDGET_WIDTH} from '../settings';
 
+/**
+ * Service for handling parsing and operations with widget matrix outside the dashboard component
+ */
 @Injectable()
 export class WidgetMatrixService {
 
+  /**
+   * Parses widgets to matrix from list
+   */
   static parseWidgets(w: WidgetInterface[]) {
     const widgets = w;
     let widgetList: WidgetInterface[][] = [];
@@ -26,10 +32,17 @@ export class WidgetMatrixService {
     return widgetList;
   }
 
+  /**
+   * Returns the last free row in matrix
+   */
   static getFreeRow(widgets: WidgetInterface[]) {
+    // TODO: make it better
     return this.parseWidgets(widgets).length;
   }
 
+  /**
+   * Creates new widget according to parameters with default width and height
+   */
   static createWidget(app: Number, account: Number, dashboard: Number, widgets) {
     return <WidgetInterface>{
       app: app, account: account, dashboard: dashboard,
