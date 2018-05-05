b0c7211d615d98f4032f1f71b66afd209b11a3b5
Simona Kurňavová <kurnavova.simona@gmail.com>
2018-05-05 16:09:28 +0200

    Error application comments

--- src/app/applications/error-application/error-popup.component.ts
+++ src/app/applications/error-application/error-popup.component.ts
@@ -3,6 +3,9 @@ import {MAPPINGS} from '../../components/main.components/application.component';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {PopupBaseComponent} from '../../components/main.components/popup-base.component';
 
+/**
+ * Popup error component
+ */
 @Component({
   selector: 'error-popup',
   templateUrl: './error-popup.component.html',
